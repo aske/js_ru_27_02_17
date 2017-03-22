69e41a5452837a15c7d1a8774ea15f9a69e61b62
Roman <kaboom.ua@gmail.com>
2017-03-22 22:52:49 +0200

    Update randomId.js

--- src/middlewares/randomId.js
+++ src/middlewares/randomId.js
@@ -4,8 +4,11 @@ import { addCommentToArticle, addCommentToStore } from '../AC';
 export default store => next => action => {
     const { type, payload } = action;
     switch (type) {
+    //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
     case ADD_COMMENT:
         const randomId = Math.floor((Math.random()) * 100000);
+        //не надо диспатчить 2 экшина, ты можешь реагировать на один в нескольких редюсерах
+        //лучше используй next, зачем store.dispatch?
         store.dispatch(addCommentToStore(payload.comment, randomId));
         store.dispatch(addCommentToArticle(payload.articleId, randomId));
     }
